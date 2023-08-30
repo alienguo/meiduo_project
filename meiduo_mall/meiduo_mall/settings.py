5ce80fbfe6cfb83232556d6f296b15937a12c6eb
daguo <daguo123@gmail.com>
2023-08-30 18:07:03 +0800

    pycharm操作git

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+# ctrl + k
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
