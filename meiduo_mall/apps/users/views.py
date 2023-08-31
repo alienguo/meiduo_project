f7dd82de7c1da81f191c7d5420ecd5e1ef3622a1
daguo <daguo123@gmail.com>
2023-08-31 15:43:03 +0800

    注册功能的实现

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 import re
+import json
 from django.views import View
 from apps.users.models import User
 from django.http import JsonResponse
@@ -24,3 +25,48 @@ class MobileCountView(View):
     def get(self, request, mobile):
         count = User.objects.filter(mobile=mobile).count()
         return JsonResponse({'code': 0, 'errmsg': 'OK', 'count': count})
+
+
+class RegisterView(View):
+
+    def post(self, request):
+        # 1.接收请求（POST---JSON）
+        body_bytes = request.body
+        body_str = body_bytes.decode()
+        body_dict = json.loads(body_str)
+
+        # 2.获取数据
+        username = body_dict.get('username')
+        password = body_dict.get('password')
+        password2 = body_dict.get('password2')
+        mobile = body_dict.get('mobile')
+        allow = body_dict.get('allow')
+
+        # 3.验证数据
+        #   3.1 用户名，密码，确认密码，手机号，是否同意协议都需要有
+        if not all([username, password, password2, mobile]):   # all是一个函数，用于判断是否数组中所有元素都为真
+            return JsonResponse({'code': 400, 'errmsg': '参数不全'})
+        #   3.2 用户名满足规则，用户名不能重复
+        if not re.match('[a-zA-Z0-9_-]{5,20}', username):
+            return JsonResponse({'code': 400, 'errmsg': '用户名不满足规则'})
+        #   3.3 密码满足规则
+        if not 20 >= len(password) >= 8:
+            return JsonResponse({'code': 400, 'errmsg': '密码不满足规则'})
+        #   3.4 确认密码和密码要一致
+        if password != password2:
+            return JsonResponse({'code': 400, 'errmsg': '两次密码不一致'})
+        #   3.5 手机号码满足规则，手机号码也不能重复
+        if not re.match(r'1[3-9]\d{9}', mobile):
+            return JsonResponse({'code': 400, 'errmsg': '手机号不满足规则'})
+        #   3.6 需要同意协议
+        if not allow:
+            return JsonResponse({'code': 400, 'errmsg': '请勾选用户使用协议'})
+
+        # 4.数据入库
+        # 密码不会加密
+        # User.objects.create(username=username, password=password, mobile=mobile)
+        # 密码加密
+        User.objects.create_user(username=username, password=password, mobile=mobile)
+
+        # 5.返回响应
+        return JsonResponse({'code': 0, 'errmsg': 'ok'})
