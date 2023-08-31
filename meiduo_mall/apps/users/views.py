a9e7d2d63d503c07525a69699d4343e2ebe04f6b
daguo <daguo123@gmail.com>
2023-08-31 09:44:53 +0800

    判断用户名是否重复的实现

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from apps.users.models import User
+from django.http import JsonResponse
+
+
+class UsernameCountView(View):
+
+    def get(self, request, username):
+        # 1.接收用户名
+        # 2.根据用户名查询数据库
+        count = User.objects.filter(username=username).count()
+        # 3.返回响应
+        return JsonResponse({'code': 0, 'count': count, 'errmsg': 'ok'})
+
