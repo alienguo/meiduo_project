d261b5d4de5092aaed340bea99a642b59da4b042
daguo <daguo123@gmail.com>
2023-08-31 09:56:42 +0800

    自定义转换器

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+import re
 from django.views import View
 from apps.users.models import User
 from django.http import JsonResponse
@@ -9,7 +10,9 @@ from django.http import JsonResponse
 class UsernameCountView(View):
 
     def get(self, request, username):
-        # 1.接收用户名
+        # 1.接收用户名，对这个用户名进行判断
+        # if not re.match('^[a-zA-Z0-9_-]{5,20}', username):
+        #     return JsonResponse({'code': 200, 'errmsg': '用户名不满足需求'})
         # 2.根据用户名查询数据库
         count = User.objects.filter(username=username).count()
         # 3.返回响应
