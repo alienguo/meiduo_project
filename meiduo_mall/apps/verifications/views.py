34f1721874aad2e50008ad4ae29b32d1e6c60b9a
daguo <daguo123@gmail.com>
2023-09-01 16:08:26 +0800

    避免频繁发送短信

--- meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/apps/verifications/views.py
@@ -98,11 +98,18 @@ class SmsCodeView(View):
         if redis_image_code.decode().lower() != image_code.lower():
             return JsonResponse({'code': 400, 'errmsg': '图片验证码错误'})
 
+        # 提取发送短信的标记，查看是否验证码还在有效期内
+        send_flag = redis_cli.get('send_flag_%s' % mobile)
+        if send_flag is not None:
+            return JsonResponse({'code': 400, 'errmsg': '不要频繁发送短信'})
         # 4.生成短信验证码
         from random import randint
         sms_code = '%04d' % randint(0, 9999)
         # 5.保存短信验证码
         redis_cli.setex(mobile, 300, sms_code)
+        # 添加发送标记,有效期60s
+        redis_cli.setex('send_flag_%s' % mobile, 60, 1)
+
         # 6.发送短信验证码
         from libs.ronglian_sms_sdk.SmsSDK import SmsSDK
         accId = '2c94811c8a27cf2d018a4c1f603a0e77'
@@ -110,5 +117,6 @@ class SmsCodeView(View):
         appId = '2c94811c8a27cf2d018a4c1f618f0e7e'
         sdk = SmsSDK(accId, accToken, appId)
         sdk.sendMessage('1', mobile, (sms_code, '5'))
+
         # 7.返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok'})
