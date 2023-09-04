3d0954dfc999f63dd9ad8f3df8f7b3a4f79ca5a8
daguo <daguo123@gmail.com>
2023-09-04 10:29:43 +0800

    省市区数据缓存的实现

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -4,20 +4,30 @@ from django.shortcuts import render
 from django.views import View
 from apps.areas.models import Area
 from django.http import JsonResponse
+from django.core.cache import cache
 
 
 class AreaView(View):
 
     def get(self, request):
-        # 1.查询省份信息
-        provinces = Area.objects.filter(parent=None)   # 查询结果集
-        # 将对象转化为字典数据
-        province_list = []
-        for province in provinces:
-            province_list.append({
-                'id': province.id,
-                'name': province.name
-            })
+
+        # 先查询缓存数据
+        province_list = cache.get('province')
+
+        if province_list is None:
+            # 1.查询省份信息
+            provinces = Area.objects.filter(parent=None)   # 查询结果集
+            # 将对象转化为字典数据
+            province_list = []
+            for province in provinces:
+                province_list.append({
+                    'id': province.id,
+                    'name': province.name
+                })
+
+            # 保存缓存数据
+            cache.set('province', province_list, 24*3600)
+
         # 2.返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'province_list': province_list})
 
@@ -25,15 +35,23 @@ class AreaView(View):
 class SubAreaView(View):
 
     def get(self, request, id):
-        up_level = Area.objects.get(id=id)
-        low_level = up_level.subs.all()
 
-        sub_data = []
-        for item in low_level:
-            sub_data.append({
-                'id': item.id,
-                'name': item.name
-            })
+        # 先查询缓存数据
+        sub_data = cache.get('city:%s' % id)
+
+        if sub_data is None:
+            up_level = Area.objects.get(id=id)
+            low_level = up_level.subs.all()
+
+            sub_data = []
+            for item in low_level:
+                sub_data.append({
+                    'id': item.id,
+                    'name': item.name
+                })
+
+            # 保存缓存数据
+            cache.set('city:%s' % id, sub_data, 24 * 3600)
 
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'sub_data': {'subs': sub_data}})
 
