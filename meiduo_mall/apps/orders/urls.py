e220b169d16ba9b4bf4feeac0cf2aa7871c3f63d
daguo <daguo123@gmail.com>
2023-09-07 10:18:14 +0800

    保存订单基本信息和订单商品信息

--- meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/apps/orders/urls.py
@@ -4,4 +4,5 @@ from apps.orders import views
 
 urlpatterns = [
     path('orders/settlement/', views.OrderSettlementView.as_view()),
+    path('orders/commit/', views.OrderCommitView.as_view()),
 ]
--- meiduo_mall/apps/orders/views.py
+++ meiduo_mall/apps/orders/views.py
@@ -1,8 +1,11 @@
+import json
+from django.utils import timezone
 from django.http import JsonResponse
 from django.shortcuts import render
 from decimal import Decimal
 from django.views import View
 from apps.goods.models import SKU
+from apps.orders.models import OrderInfo, OrderGoods
 from apps.users.models import Address
 from utils.views import LoginRequiredJSONMixin
 from django_redis import get_redis_connection
@@ -68,4 +71,87 @@ class OrderSettlementView(LoginRequiredJSONMixin, View):
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'context': context})
 
 
+class OrderCommitView(LoginRequiredJSONMixin, View):
+    """订单提交"""
 
+    def post(self, request):
+        """保存订单信息和订单商品信息"""
+        # 获取当前要保存的订单数据
+        json_dict = json.loads(request.body.decode())
+        address_id = json_dict.get('address_id')
+        pay_method = json_dict.get('pay_method')
+        # 校验参数
+        if not all([address_id, pay_method]):
+            return JsonResponse({'code': 400, 'errmsg': '缺少必传参数}'})
+        # 判断address_id是否合法
+        try:
+            address = Address.objects.get(id=address_id)
+        except Exception:
+            return JsonResponse({'code': 400, 'errmsg': '参数address_id错误'})
+        # 判断pay_method是否合法
+        if pay_method not in [OrderInfo.PAY_METHODS_ENUM['CASH'], OrderInfo.PAY_METHODS_ENUM['ALIPAY']]:
+            return JsonResponse({'code': 400, 'errmsg': '参数pay_method错误'})
+
+        # 获取登录用户
+        user = request.user
+        # 生成订单编号：年月日时分秒+用户编号
+        order_id = timezone.localtime().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
+        # 保存订单基本信息 OrderInfo（一）
+        order = OrderInfo.objects.create(
+            order_id=order_id,
+            user=user,
+            address=address,
+            total_count=0,
+            total_amount=Decimal('0'),
+            freight=Decimal('10.00'),
+            pay_method=pay_method,
+            status=OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM['ALIPAY'] else
+            OrderInfo.ORDER_STATUS_ENUM['UNSEND']
+        )
+        # 从redis读取购物车中被勾选的商品信息
+        redis_cli = get_redis_connection('carts')
+        redis_cart = redis_cli.hgetall('carts_%s' % user.id)
+        selected = redis_cli.smembers('selected_%s' % user.id)
+        carts = {}
+        for sku_id in selected:
+            carts[int(sku_id)] = int(redis_cart[sku_id])
+        sku_ids = carts.keys()
+
+        # 遍历购物车中被勾选的商品信息
+        for sku_id in sku_ids:
+            # 查询SKU信息
+            sku = SKU.objects.get(id=sku_id)
+            # 判断SKU库存
+            sku_count = carts[sku.id]
+            if sku_count > sku.stock:
+                return JsonResponse({'code': 400, 'errmsg': '库存不足'})
+
+            # SKU减少库存，增加销量
+            sku.stock -= sku_count
+            sku.sales += sku_count
+            sku.save()
+
+            # 保存订单商品信息 OrderGoods（多）
+            OrderGoods.objects.create(
+                order=order,
+                sku=sku,
+                count=sku_count,
+                price=sku.price,
+            )
+
+            # 保存商品订单中总价和总数量
+            order.total_count += sku_count
+            order.total_amount += (sku_count * sku.price)
+
+        # 添加邮费和保存订单信息
+        order.total_amount += order.freight
+        order.save()
+
+        # 清除购物车中已结算的商品
+        pl = redis_cli.pipeline()
+        pl.hdel('carts_%s' % user.id, *selected)
+        pl.srem('selected_%s' % user.id, *selected)
+        pl.execute()
+
+        # 响应提交订单结果
+        return JsonResponse({'code': 0, 'errmsg': '下单成功', 'order_id': order.order_id})
